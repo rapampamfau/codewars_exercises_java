98aae823c2333f24441bcb12ff5a8dba1cd57d48
rapampamfau <rafal.szczepanczyk7@gmail.com>
2023-01-16 22:07:09 +0100

    Main class update

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import org.example.excercises.DescendingOrder;
+import org.example.excercises.ListFilteringOne;
+
+import java.util.List;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        //@Username Validation
+        //    System.out.println(UsernameValidation.usernameValidator("_tururur"));
+
+        //@String Reverse
+        //    System.out.println(StringReverse.firstReverse("bonjour"));
+
+        //@Find Intersection
+        //    System.out.println(FindIntersection.findIntersection(new String[] {"1, 3, 4, 7, 13", "1, 2, 4, 13, 15"})); //TODO
+
+        //@Create Phone Number
+        //    System.out.println(PhoneNumberCreation.createPhoneNumber(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}));
+
+        //@List Filtering One
+        //        System.out.println(ListFilteringOne.filterList(List.of(1, 2, "a", "b", "aasf", "1", "123", 231)));
+
+        //@Descending order
+        //        System.out.println(DescendingOrder.sortDesc(324238));
+
+    }
+}
